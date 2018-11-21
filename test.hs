4b112f39c7e98de3cb9494c2d3902aaaa5a7df04
Anton Vlasik <snumber29@gmail.com>
2018-11-22 00:15:04 +0300

    added gitignore and some haskell examples

--- test.hs
+++ test.hs
@@ -7,3 +7,9 @@ doubleSamllNumber x = if x > 100 then x else x*2
 replacer list = [if x <= 5 then 'A' else 'Z' | x <- list, x `mod` 2 == 0]
 
 twoListsProductionFiltered list1 list2 = [x*y | x <- list1, y <- list2, x < 50]
+
+customLength list = sum [1 | _ <- list]
+
+trimLowerCaseLetters string = [c | c <- string, c `elem` ['A'..'Z']]
+
+leftEvensInNestedLists wrapperList = [[x | x <- nestedList, even x] | nestedList <- wrapperList]
