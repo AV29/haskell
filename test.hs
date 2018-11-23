61a0418f97525b2e1a815b1a0a6f0b1006e1a9b3
Anton Vlasik <snumber29@gmail.com>
2018-11-23 14:35:07 +0300

    pattern matching

--- test.hs
+++ test.hs
@@ -15,3 +15,19 @@ trimLowerCaseLetters string = [c | c <- string, c `elem` ['A'..'Z']]
 leftEvensInNestedLists wrapperList = [[x | x <- nestedList, even x] | nestedList <- wrapperList]
 
 findRightTriangles = [(a, b, c) | c <- [1..500], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2, a + b + c == 1000]
+
+factorial::(Integral a) => a -> a
+factorial 0 = 1
+factorial x = x * factorial(x - 1)
+
+fibbonachi::(Integral a) => a -> a
+fibbonachi 0 = 0
+fibbonachi 1 = 1
+fibbonachi x = fibbonachi(x-1) + fibbonachi(x-2)
+
+addVectors::(Num a, Num a) => (a, a) -> (a, a) -> (a, a)
+addVectors (a, b) (c, d) = (a + c, b + d)
+
+customSum::(Num a) => [a] -> a
+customSum [] = 0
+customSum (x:rest) = x + customSum rest
