381427be206a32c9f1a3ba45facaf2d4a4112645
Anton Vlasik <snumber29@gmail.com>
2018-12-28 16:17:17 +0300

    added collatz

---
+++ collatz.hs
@@ -0,0 +1,9 @@
+chain :: (Integral a) => a -> [a]
+chain 1 = [1]
+chain n
+    | even n = n:chain (n `div` 2)
+    | odd n = n:chain (n*3 + 1)
+
+
+numLongChains :: Int
+numLongChains = length (filter (\chainEls -> length chainEls > 15) (map chain [1..100]))
